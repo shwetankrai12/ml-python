7ebd0d1af2beb1fac6b3874e4f809ddd26627268
shwetankrai12 <shwetankrai12102004@gmail.com>
2026-03-12 11:56:06 +0530

    scopes

---
+++ basics/scopes/01_scopes.py
@@ -0,0 +1,48 @@
+username = "shwetank"
+
+def func():
+    username = "chai"
+    print (username)
+
+print (username)
+func()
+
+
+x = 99
+
+# def func2(y):
+#     z = x + y
+#     return z
+
+# result = func2(1)
+# print(result)
+
+# def func3():
+#     global x #avoid krna chaiye aisa
+#     x = 88
+
+# func3()
+# print(x)
+
+
+def f1():
+    x = 88
+    def f2():
+        print(x)
+
+    return f2
+myresult = f1()
+myresult()
+
+
+def chaicoder(num):
+    def actual (x):
+        return x ** num
+    return actual
+
+
+f = chaicoder(2)
+g = chaicoder(3)
+
+print(f(3))
+print(g(3))
